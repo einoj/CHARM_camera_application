e547493e7ab88a07c7229a6145c8dd6e24ca8608
Eino Juhani Oltedal <e.j.oltedal@fys.uio.no>
2014-10-09 15:40:58 +0200

    initial commit, working video capture, no graphical functions

---
+++ videoTest.py
@@ -0,0 +1,27 @@
+import numpy as np
+import cv2
+import os
+
+if os.name == 'nt':
+    cap = cv2.VideoCapture(1)
+else:
+    cap = cv2.VideoCapture(0)
+
+#cap2 = cv2.VideoCapture(2)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    #ret2, frame2 = cap2.read()
+
+    # Our operations on the frame come here
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('Camera 1',frame) #cv2.imshow('frame2',frame2)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
